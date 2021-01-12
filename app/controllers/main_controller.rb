e021205eb42e880521cc821d13911a2da9aa9c5a
Pabsalonso <56518151+Pabsalonso@users.noreply.github.com>
2021-01-12 21:30:32 +0000

    Add files via upload
    Acabo de subir la carpeta app del projecto en rails,(no me dejaba subir el projecto entero, asi que solo subo lo importante, la carpeta y las gemas necesarias). Por lo pronto tenemos el esqueleto de la aplicacion:
    Ya carga las paginas con sus respectivos estilos, y los links e imagenes se han adaptado para que funcionen en rails.
    Ahora me pondre a trabajar con la lectura y escritura del json, y una vez tengamos eso nos ponemos con la carga dinamica.
    Tambien he mirado el tratamiento de sesiones para iniciar sesion. Deberiamos añadir en la carta unaseccion UPDATE haciendo uso de ruby para que un usuario admin pueda cambiar cosas del json

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,16 @@
+class MainController < ApplicationController
+	def index
+	end
+	def carta
+	end
+	def ofertas
+	end
+	def carrito
+	end
+	def signUp
+	end
+	def login
+	end
+	def account
+	end
+end
